803d3a63177aab4496d7fdd772dcd389b8593dae
Rootul Patel <patelrootul1@gmail.com>
2023-05-04 21:59:44 -0400

    Name changes at every location except license

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,18 +12,14 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>Rootul&apos;Patel</h2>
+        <p><a href="mailto:rooatwork1@gmail.com">rooatwork1@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Rootul. I am a <a href="https://www.srmist.edu.in/department/department-of-computer-science/">SRM Institute Of Science and Technology</a> graduate, Tata Consultancy Services Alumni
       </p>
       <ul className="actions">
         <li>
@@ -34,7 +30,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Rootul Patel .</p>
     </section>
   </section>
 );
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,8 +5,8 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'}
+    description={"Rootul Patel's personal website. Waterloo based Software developer, "
+    + 'SRMIST and Tata Consultancy services Alumni '}
   >
     <article className="post" id="index">
       <header>
