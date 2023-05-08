da16bb1c4aa8a97fa123f246e04c3a1651c7f796
Rootul Patel <patelrootul1@gmail.com>
2023-05-08 14:55:37 -0400

    Resume Updated with few things left

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,15 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'Conestoga College',
+    degree: 'Mobile Solutions Development',
+    link: 'https://www.conestogac.on.ca/fulltime/mobile-solutions-development',
+    year: 2022,
   },
   {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'SRM Institute Of Science And Technology',
+    degree: 'B.Tech Computer Science And Engineering',
+    link: 'https://www.srmist.edu.in/',
+    year: 2017,
   },
 ];
 
