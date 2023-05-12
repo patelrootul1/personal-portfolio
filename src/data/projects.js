eb1f37d4d71eaa86c4b0ebe3531ad04b51df76e6
Rootul Patel <patelrootul1@gmail.com>
2023-05-12 12:50:36 -0400

    Added 1 more project

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,9 +7,20 @@ const data = [
     image: '/images/projects/spacepotato.jpg',
     date: '2021-05-22',
     desc:
-      'Implemented Supervised Learning machine learning for electricity comsumption prediction '
-      + 'Implemented the models using Python, Jupyter Notebooks and hosted it on Heroku '
+      'Implemented Supervised Learning machine learning for electricity comsumption prediction;'
+      + 'Implemented the models using Python, Jupyter Notebooks and hosted it on Heroku;'
       + 'The Accuracy of the model was 88%',
+  },
+  {
+    title: 'Reaction timer using Vue.js',
+    subtitle: 'JavaScript Framework-Vue.js',
+    link: 'https://reaction-timer-1796f.web.app/',
+    image: '/images/projects/reactiontimer.jpg',
+    date: '2023-04-16',
+    desc:
+      'Implemented Vue.js Framework for making simple Reaction timer app;'
+      + 'Used Random function between 2-7 seconds to randomly generate a component to allow;'
+      + 'It will allow users to improve their reactions',
   },
 ];
 
